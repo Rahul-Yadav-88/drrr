87cfb1b164c5a0661db314253dd9d099a8e25979
Nikhil <nikhilbatiyal3891@gmail.com>
2025-11-26 19:02:49 +0530

    changes

--- src/components/home/section2.jsx
+++ src/components/home/section2.jsx
@@ -5,7 +5,7 @@ export default function Section2() {
     <section className="w-full bg-white py-16 px-6 md:px-20">
       <div className="max-w-7xl mx-auto grid md:grid-cols-2 gap-12">
 
-        {/* LEFT NAME + DEGREE */}
+      
         <div>
           <h2 className="text-[#5B8291] text-2xl md:text-3xl font-bold leading-snug">
             Dr. 
@@ -17,7 +17,7 @@ export default function Section2() {
         </div>
 
 
-        {/* RIGHT PARAGRAPHS */}
+      
         <div className="text-[#5B8291] text-[20px] leading-relaxed">
           <p>
             Dr. Gaurav has over 28 years of post-specialization experience across India, Kuwait, and the UAE.
